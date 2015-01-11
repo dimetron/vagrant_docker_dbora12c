0777f8e78f43a1eebe487cd649bd87342237a02a
Dmytro Rashko <dimetron@me.com>
2015-01-11 18:16:42 +0100

    Docker Temp folder settings for import/export

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -10,31 +10,32 @@ rm -rf /vagrant/docker_export/*.tar.xz
 
 #to reduce internal tmp usage after image load we will use external tmp
 echo "export DOCKER_TMPDIR=$DOCKER_TMPDIR" >> /etc/sysconfig/docker
+echo "export DOCKER_TMPDIR=$DOCKER_TMPDIR" >> /etc/profile.d/temp.sh
 service docker restart
 
 echo ">>> Loading base OL6.6 image ..."
-sudo  su -c "DOCKER_TMPDIR=$DOCKER_TMPDIR && docker load -i /vagrant/downloads/oraclelinux-6.6.tar.xz"
+sudo  su -c "docker load -i /vagrant/downloads/oraclelinux-6.6.tar.xz"
 
 echo "@@@ Building Java7 image ..."
 cp /etc/yum.conf /vagrant/docker/java-7u71/
 sudo docker build --no-cache -t="oracle/java-7u71" /vagrant/docker/java-7u71/
-sudo docker run  --privileged -h jdk7h --name java -t -d oracle/java-7u71 /bin/bash
-sudo  su -c "DOCKER_TMPDIR=$DOCKER_TMPDIR && docker export java | docker import - oracle/java"
-sudo docker kill java && docker rm java
+sudo docker run   --privileged -h jdk7h --name java -t -d oracle/java-7u71 /bin/bash
+sudo  su -c "docker export java |  docker import - oracle/java"
+sudo  su -c "docker kill   java && docker rm java"
 
 echo "@@@ Building Weblogic image ..."
 cp /etc/yum.conf /vagrant/docker/weblogic-12/
 sudo docker build --no-cache -t="oracle/weblogic12" /vagrant/docker/weblogic-12/
-sudo docker run  --privileged -h crm92 --name weblogic  -p 8001:8001 -p 8002:8002 -t -d oracle/weblogic12 /bin/bash
-sudo  su -c "DOCKER_TMPDIR=$DOCKER_TMPDIR && docker export weblogic | docker import - oracle/weblogic"
-sudo docker kill weblogic && docker rm weblogic
+sudo docker run   --privileged -h crm92 --name weblogic  -p 8001:8001 -p 8002:8002 -t -d oracle/weblogic12 /bin/bash
+sudo  su -c "docker export weblogic |  docker import - oracle/weblogic"
+sudo  su -c "docker kill   weblogic && docker rm weblogic"
 
 echo "@@@ Building OracleDB12c image ..."
 cp /etc/yum.conf /vagrant/docker/oracle-c12/
 sudo docker build --no-cache -t="oracle/oracle12c" /vagrant/docker/oracle-c12/
-sudo docker run --privileged -h db12c --name database -p 1521:1521 -v /vagrant:/vagrant oracle/oracle12c /bin/bash /vagrant/scripts/install_oracle.sh
-sudo  su -c "DOCKER_TMPDIR=$DOCKER_TMPDIR && docker export database | docker import - oracle/database"
-sudo docker kill database && docker rm database
+sudo docker run   --privileged -h db12c --name database -p 1521:1521 -v /vagrant:/vagrant oracle/oracle12c /bin/bash /vagrant/scripts/install_oracle.sh
+sudo  su -c "docker export database |  docker import - oracle/database"
+sudo  su -c "docker kill   database && docker rm database"
 
 echo "Installation complete"
 sudo docker ps -a
@@ -50,4 +51,3 @@ echo " └─-> oracle/database ..." && sudo  su -c "DOCKER_TMPDIR=$DOCKER_TMPDI
 echo " ~~~"
 ls -lstr /vagrant/docker_export/
 echo "Export complete"
-
