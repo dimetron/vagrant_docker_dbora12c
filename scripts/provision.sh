ca4169eb1c647ff9238c9d6edcd2c94c6fd94cd7
Dmytro Rashko <dimetron@me.com>
2015-01-11 18:46:22 +0100

    Docker import/export optimizations

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -22,7 +22,7 @@ sudo docker build --no-cache -t="oracle/java-7u71" /vagrant/docker/java-7u71/
 sudo docker run   --privileged -h jdk7h --name java -t -d oracle/java-7u71 /bin/bash
 sudo su -c "docker export java |  docker import - oracle/java"
 sudo su -c "docker kill   java && docker rm java && docker rmi oracle/java-7u71"
-echo "Exporting oracle/java docker images to [docker_export]:"
+echo ">> Exporting oracle/java docker images to [docker_export]:"
 echo " └─ ...-> oracle/java .  " && sudo  su -c "docker save -o /vagrant/docker_export/java-7u71.tar.xz oracle/java"
 
 echo "@@@ Building Weblogic image ..."
@@ -31,8 +31,9 @@ sudo docker build --no-cache -t="oracle/weblogic12" /vagrant/docker/weblogic-12/
 sudo docker run   --privileged -h crm92 --name weblogic  -p 8001:8001 -p 8002:8002 -t -d oracle/weblogic12 /bin/bash
 sudo su -c "docker export weblogic |  docker import - oracle/weblogic"
 sudo su -c "docker kill   weblogic && docker rm weblogic && docker rmi oracle/weblogic12"
-echo "Exporting oracle/weblogic images to [docker_export]:"
+echo ">> Exporting oracle/weblogic images to [docker_export]:"
 echo " └─ ...-> oracle/weblogic .. " && sudo  su -c "docker save -o /vagrant/docker_export/weblogic12.tar.xz oracle/weblogic"
+
 
 echo "@@@ Building OracleDB12c image ..."
 cp /etc/yum.conf /vagrant/docker/oracle-c12/
@@ -40,6 +41,8 @@ sudo docker build --no-cache -t="oracle/oracle12c" /vagrant/docker/oracle-c12/
 sudo docker run   --privileged -h db12c --name database -p 1521:1521 -v /vagrant:/vagrant oracle/oracle12c /bin/bash /vagrant/scripts/install_oracle.sh
 sudo su -c "docker export database |  docker import - oracle/database"
 sudo su -c "docker kill   database && docker rm database && docker rmi oracle/oracle12c"
+echo ">> Exporting oracle/database images to [docker_export]:"
+echo " └─ ...-> oracle/database .. " && sudo  su -c "docker save -o /vagrant/docker_export/oracle12c_db.tar oracle/database"
 
 echo "Installation complete"
 sudo docker ps -a
